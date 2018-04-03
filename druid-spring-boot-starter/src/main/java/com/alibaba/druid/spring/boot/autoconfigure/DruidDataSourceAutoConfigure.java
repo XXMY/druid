86221476a3a722f91fb0840fbaa5b240e1f9cac4
caifw <caifw@wasu.com>
2018-04-03 09:16:18 +0800

    Druid 多客户端监控改造

--- druid-spring-boot-starter/src/main/java/com/alibaba/druid/spring/boot/autoconfigure/DruidDataSourceAutoConfigure.java
+++ druid-spring-boot-starter/src/main/java/com/alibaba/druid/spring/boot/autoconfigure/DruidDataSourceAutoConfigure.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2017 Alibaba Group Holding Ltd.
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,10 @@
  */
 package com.alibaba.druid.spring.boot.autoconfigure;
 
+import javax.sql.DataSource;
+
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.spring.boot.autoconfigure.metadata.DruidDataSourcePoolMetadata;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceWrapper;
 import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
 import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidFilterConfiguration;
 import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidSpringAopConfiguration;
@@ -25,52 +27,32 @@ import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidWebStatFilterConfig
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.autoconfigure.jdbc.metadata.DataSourcePoolMetadata;
-import org.springframework.boot.autoconfigure.jdbc.metadata.DataSourcePoolMetadataProvider;
-import org.springframework.boot.autoconfigure.jdbc.metadata.DataSourcePoolMetadataProvidersConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import javax.sql.DataSource;
-
 /**
  * @author lihengming [89921218@qq.com]
  */
 @Configuration
-@ConditionalOnClass(com.alibaba.druid.pool.DruidDataSource.class)
+@ConditionalOnProperty(prefix = "spring.datasource", name = "initialize", havingValue = "true", matchIfMissing = true)
+@ConditionalOnClass(DruidDataSource.class)
 @AutoConfigureBefore(DataSourceAutoConfiguration.class)
-@EnableConfigurationProperties({DruidStatProperties.class, DataSourceProperties.class})
+@EnableConfigurationProperties({DruidStatProperties.class,DataSourceProperties.class})
 @Import({DruidSpringAopConfiguration.class,
-        DruidStatViewServletConfiguration.class,
-        DruidWebStatFilterConfiguration.class,
-        DruidFilterConfiguration.class})
+    DruidStatViewServletConfiguration.class,
+    DruidWebStatFilterConfiguration.class,
+    DruidFilterConfiguration.class
+})
 public class DruidDataSourceAutoConfigure {
 
-    @Bean
+    @Bean(initMethod = "init")
     @ConditionalOnMissingBean
     public DataSource dataSource() {
         return new DruidDataSourceWrapper();
-    }
-
-    /**
-     * Register the {@link DataSourcePoolMetadataProvider} instances to support DataSource metrics.
-     *
-     * @see DataSourcePoolMetadataProvidersConfiguration
-     */
-    @Bean
-    public DataSourcePoolMetadataProvider druidDataSourcePoolMetadataProvider() {
-        return new DataSourcePoolMetadataProvider() {
-            @Override
-            public DataSourcePoolMetadata getDataSourcePoolMetadata(DataSource dataSource) {
-                if (dataSource instanceof DruidDataSource) {
-                    return new DruidDataSourcePoolMetadata((DruidDataSource) dataSource);
-                }
-                return null;
-            }
-        };
     }
 }
